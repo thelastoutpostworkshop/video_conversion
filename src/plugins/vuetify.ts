b2bbfbd927999bf19099e03c7fea1b88e7e51bf3
thelastoutpostworkshop <thelastoutpostworkshop@gmail.com>
2026-02-21 06:19:06 -0500

    ui

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,11 @@
-﻿import { createVuetify } from "vuetify";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
 
 export const vuetify = createVuetify({
+  components,
+  directives,
   icons: {
     defaultSet: "mdi",
     aliases,
